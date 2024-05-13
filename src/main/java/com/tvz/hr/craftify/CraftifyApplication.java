18492da5f29d8030f802dbb3560c29dc61516cda
Ana Konjetić <akonjetic@tvz.hr>
2024-05-13 09:59:14 +0200

    initial

---
+++ src/main/java/com/tvz/hr/craftify/CraftifyApplication.java
@@ -0,0 +1,13 @@
+package com.tvz.hr.craftify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvz/hr/craftify/CraftifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvz.hr.craftify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
