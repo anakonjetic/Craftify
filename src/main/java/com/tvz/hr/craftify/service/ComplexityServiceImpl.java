c375e9092c4bb8da34c71f91ec8985a2c50c49c0
kristinaa14 <kristina.anicic1@gmail.com>
2024-05-15 20:00:11 +0200

    CRAFT - 139 - 1 -> REST API Endpoints for Complexity done

---
+++ src/main/java/com/tvz/hr/craftify/service/ComplexityServiceImpl.java
@@ -0,0 +1,46 @@
+package com.tvz.hr.craftify.service;
+
+import com.tvz.hr.craftify.model.Complexity;
+import com.tvz.hr.craftify.model.Project;
+import com.tvz.hr.craftify.repository.ComplexityRepository;
+import com.tvz.hr.craftify.service.dto.ComplexityDTO;
+import com.tvz.hr.craftify.service.dto.ComplexityGetDTO;
+import com.tvz.hr.craftify.service.dto.ProjectDTO;
+import com.tvz.hr.craftify.utilities.MapToDTOHelper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.tvz.hr.craftify.utilities.MapToDTOHelper.mapToComplexityGetDTO;
+
+@Service
+@AllArgsConstructor
+public class ComplexityServiceImpl implements ComplexityService {
+    ComplexityRepository complexityRepository;
+
+    public List<ComplexityDTO> getAllComplexities() {
+        List<Complexity> complexities = complexityRepository.findAll();
+        return complexities.stream()
+                .map(MapToDTOHelper::mapToComplexityDTO)
+                .collect(Collectors.toList());
+    };
+    public Optional<ComplexityGetDTO> getComplexityById(Long id) {
+        Optional<Complexity> optionalComplexity = complexityRepository.findById(id);
+        return optionalComplexity.map(MapToDTOHelper::mapToComplexityGetDTO);
+    };
+    public Complexity createComplexity(Complexity complexity) { return complexityRepository.save(complexity); };
+    public Complexity updateComplexity(Complexity complexity, Long id) {
+        Optional<Complexity> optionalComplexity = complexityRepository.findById(id);
+        if (optionalComplexity.isEmpty()) {
+            return null;
+        }
+        Complexity existingComplexity = optionalComplexity.get();
+        existingComplexity.setName(complexity.getName());
+
+        return complexityRepository.save(existingComplexity);
+    };
+    public void deleteComplexity(Long id) { complexityRepository.deleteById(id); };
+}
---
+++ src/main/java/com/tvz/hr/craftify/service/dto/ComplexityDTO.java
@@ -0,0 +1,15 @@
+package com.tvz.hr.craftify.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ComplexityDTO {
+    private Long id;
+    private String name;
+}
